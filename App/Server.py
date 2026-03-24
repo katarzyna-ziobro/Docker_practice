6ac19e0ad5ea15694cd0b1b0429d163358fed689
Katarzyna Ziobro <katarzyna.n.ziobro@gmail.com>
2026-03-24 11:55:34 +0100

    docker compose

--- App/Server.py
+++ App/Server.py
@@ -18,25 +18,43 @@ def random_number():
 
 class Request_Handler(BaseHTTPRequestHandler):
     
+ 
     def do_GET(self):
         with open(HTML_FILE, "r") as f:
             content = f.read()
         
         lucky_number = random_number()
+
         content = content.replace("{{NUMBER}}", str(lucky_number))
+
+        db = DB_conn()
+
+        last_numbers = db.get_last_10()
+        numbers_html = "".join(f"<li>{num}</li>" for num in last_numbers)
+
+        content = content.replace("{{LAST_NUMBERS}}", numbers_html)
        
+        db.insert_lucky_number(number=lucky_number)
 
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
         self.wfile.write(content.encode())
-        db = DB_conn()
-        db.insert_lucky_number(number=lucky_number)
+
+    # def do_GET(self):
+    #     self.send_response(200)
+    #     self.send_header("Content-type", "text/html")
+    #     self.end_headers()
+
+    #     with open(HTML_FILE, "r") as f:
+    #         content = f.read()
+    #     self.wfile.write(content.encode())
+
+
 
 
 if __name__ == "__main__":
     server = HTTPServer((HOST, PORT), Request_Handler)
     print(f"Server is running on {HOST}:{PORT}")
     server.serve_forever()
-        
